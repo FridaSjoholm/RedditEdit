b052f7a481b0ba23b439da02e53abc1d11ad63a0
Frida Sjoholm <frida@fridaesign.se>
2017-03-09 15:45:03 -0800

    Edits header

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 $('head').append('<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">')
-$('.title').css( "color", "lightslategrey" )
-$('img').css( "border-radius","35px" )
-$('.title').css( "font-family", 'Raleway' )
+$('head').append('<link href="https://fonts.googleapis.com/css?family=Nunito:600" rel="stylesheet">')
+
+$('body').css({"font-family":"Raleway"})
+$('img').css({"border-radius":"35px", "width":"70px", "height":"70px"} )
+$('.title').css({"font-size":'1.2em', "color":"black"})
+$('.score').css({"font-size":'1.2em', "color":"#FF4500"})
+$('.rank').css({"font-size":'1.8em', "color":"gray"})
+
+$("#header-bottom-left").css({"background-color":"white", "padding-top": "25px"})
+$(".tabmenu li a").css({"color":"black","font-family":"Raleway","font-size":'1.2em'})
+$("#header-bottom-right").css({"background-color":"#FF4500", "border-radius":"0px",})
+$("#header-bottom-right .separator").html("<br>")
+$("#header-bottom-right a").css({"font-size":'1.1em', "color":"black", "text-align":"right"})
